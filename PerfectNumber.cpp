5cb99cd72e0f1a6281342e8907d07bc04f06a1c9
kimmypracha <pracha.promtaow@gmail.com>
2018-03-09 15:38:57 +0700

    Add files via upload

---
+++ PerfectNumber.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+ using namespace std;
+
+   int main(){
+        int N , Sum = 0;
+        printf("Enter a number: ");
+        scanf("%d" , &N);
+        for(int i = 1 ; i <= N ;++i){
+            if(N%i == 0){
+                Sum += i;
+            }
+        }
+        if(Sum == N)printf("%d is a perfect number",N);
+        else printf("%d is not a perfect number" , N);
+   }
